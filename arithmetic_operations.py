05e4280d9c6ecd28284b496dbd8eab502c4e5998
hastingsaustin <hastings.austin@student.ccm.edu>
2026-09-17 16:50:02 -0400

    Complete Week 4 Lab 2

---
+++ arithmetic_operations.py
@@ -0,0 +1,31 @@
+#Austin Hastings
+#CMP 131-86520
+#Week 4 
+#Lab 2
+#Assignment 4
+#17 Sep 2026
+print("--------------------")
+print("Basic Math operations")
+print("--------------------")
+num_1 = float(input("Enter the Number 12:"))
+num_2 = float(input("Enter The Number 4:"))
+print()
+addition= num_1 + num_2
+print("----------------------")
+subtraction= num_1 - num_2
+print("----------------------")
+multiplication= num_1 * num_2
+print("----------------------")
+division= num_1 / num_2
+print("----------------------")
+power= num_1 ** num_2
+print("----------------------")
+average= (num_1 + num_2) / num_2
+print("----------------------")
+print()
+print("Addition Result:", f"{addition:.2f}")
+print("Subtraction Result", f"{subtraction:.2f}")
+print("Multiplication Result:", f"{multiplication:.2f}")
+print("Division Result:", f"{division:.2f}")
+print("Power Result:", f"{power:.2f}")
+print("Average Result:", f"{average:.2f}")
